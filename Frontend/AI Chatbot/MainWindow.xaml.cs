8e7e5a18f4ab0416b0434bea58530fdc1a529920
gundamMC <gundammc@qq.com>
2018-05-20 16:20:15 -0700

    Optimizes visuals

--- Frontend/AI Chatbot/MainWindow.xaml.cs
+++ Frontend/AI Chatbot/MainWindow.xaml.cs
@@ -42,17 +42,19 @@ namespace AI_Chatbot
                 input_field.Text = "Input here";
         }
         
-        private void send_button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+
+        private void send_button_Click(object sender, RoutedEventArgs e)
         {
             if (input_field.Text == "Input here" || input_field.Text == "")
                 return;
-            ChatBubbleControl input = new ChatBubbleControl();
-            input.Text = input_field.Text;
-            input.IsUser = true;
-            Label placeHolder = new Label();
-            placeHolder.Height = 40;
+
+            // Testing
+            ChatBubbleControl input = new ChatBubbleControl() { IsUser = true, Text = input_field.Text };
+
+            // no need for margins since it will be a strickly one-to-one conversation
+            // (the user will not be able to send 2 messages in a row)
             conversation_stack_panel.Children.Add(input);
-            conversation_stack_panel.Children.Add(placeHolder);
+
             input_field.Text = "Input here";
         }
     }

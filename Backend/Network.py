c1dcb99462433f3fa43c51e7a659f13f1874a635
gundamMC <gundammc@qq.com>
2018-05-21 13:08:57 -0700

    Finishes network

--- Backend/Network.py
+++ Backend/Network.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
+import numpy as np
+import WordEmbedding
 
 
 class ChatbotNetwork:
 
-    def __init__(self, learning_rate=0.01, batch_size=256, vector_count=20000, vector_length=53):
+    def __init__(self, learning_rate=0.01, batch_size=256, vector_count=20000, vector_length=54):
         # hyperparameters
         self.learning_rate = learning_rate
         self.batch_size = batch_size
@@ -11,51 +13,139 @@ class ChatbotNetwork:
         # Network hyperparameters
         self.n_vector = vector_length  # glove 50 + 3 special tokens
         self.vector_count = vector_count
-        self.max_seuqence = 50
+        self.max_sequence = 50
         self.n_hidden = 128
 
         # Tensorflow placeholders
-        self.x = tf.placeholder("float", [None, self.max_seuqence])
+        self.x = tf.placeholder("float", [None, self.max_sequence])
         self.x_length = tf.placeholder("float", [None])
-        self.y = tf.placeholder("float", [None, self.max_seuqence])
+        self.y = tf.placeholder("float", [None, self.max_sequence])
         self.y_length = tf.placeholder("float", [None])
 
         # Network parameters
         self.cell_encode = tf.contrib.rnn.BasicLSTMCell(self.n_hidden)
         self.cell_decode = tf.contrib.rnn.BasicLSTMCell(self.n_hidden)
 
-    def set_training_data(self, data):
-        pass
+        # Optimization
+        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=self.y, logits=self.network())
+        self.cost = (tf.reduce_sum(crossent) / self.batch_size)
+        self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
+
+        # Tensorflow initialization
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+        self.sess.run(tf.global_variables_initializer())
+
+        # Word embedding
+        self.word_embedding = tf.Variable(tf.constant(0.0, shape=WordEmbedding.embeddings.shape), trainable=False)
+        embedding_placeholder = tf.placeholder(tf.float32, shape=WordEmbedding.embeddings.shape)
+        self.sess.run(self.word_embedding.assign(embedding_placeholder),
+                      feed_dict={embedding_placeholder: WordEmbedding.embeddings})
 
     def network(self, mode="train"):
+        embedded_x = tf.nn.embedding_lookup(self.word_embedding, self.x)
+
         encoder_out, encoder_states = tf.nn.dynamic_rnn(
             self.cell_encode,
-            inputs=self.x,
+            inputs=embedded_x,
             dtype=tf.float32,
             sequence_length=self.x_length,
             swap_memory=True)
+
+        projection_layer = tf.layers.Dense(self.vector_count)
 
         if mode == "train":
             helper = tf.contrib.seq2seq.TrainingHelper(
                 inputs=self.y,
                 sequence_length=self.y_length
             )
+
+            decoder = tf.contrib.seq2seq.BasicDecoder(
+                self.cell_decode,
+                helper,
+                encoder_states,
+                output_layer=projection_layer
+            )
+
         else:
-            helper = None
 
-        decoder = tf.contrib.seq2seq.BasicDecoder(
-            self.cell_decode,
-            helper,
-            encoder_states,
-            output_layer=None  # projection layer
-        )
+            # Replicate encoder infos beam_width times
+            decoder_initial_state = tf.contrib.seq2seq.tile_batch(
+                encoder_states, multiplier=3)
 
-        outputs, _ = tf.contrib.seq2seq.dynamic_decode(decoder, impute_finished=False)
+            decoder = tf.contrib.seq2seq.BeamSearchDecoder(
+                        cell=self.cell_decode,
+                        embedding=self.word_embedding,
+                        start_tokens=tf.tile(WordEmbedding.start, [self.x.shape[0]]),
+                        end_token=WordEmbedding.end,
+                        initial_state=decoder_initial_state,
+                        beam_width=3,
+                        output_layer=projection_layer,
+                        length_penalty_weight=0.0
+            )
 
-        return outputs
+        outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder, impute_finished=False)
 
-    def train(self, epochs=500, display_step=50):
-        pass
+        logits = outputs.rnn_output
+
+        return logits
+
+    def train(self, train_x, x_length, train_y, y_length, epochs=500, display_step=50):
+        for epoch in range(epochs+1):
+            mini_batches_x, mini_batches_x_length, mini_batches_y, mini_batches_y_length \
+                = self.random_mini_batches([train_x, x_length, train_y, y_length], self.batch_size)
+
+            for batch in range(len(mini_batches_x)):
+                batch_x = mini_batches_x[batch]
+                batch_x_length = mini_batches_x_length[batch]
+                batch_y = mini_batches_y[batch]
+                batch_y_length = mini_batches_y_length[batch]
+
+                self.sess.run(self.train_op, feed_dict={
+                    self.x: batch_x,
+                    self.x_length: batch_x_length,
+                    self.y: batch_y,
+                    self.y_length: batch_y_length
+                })
+
+                if epoch % display_step == 0:
+                    cost_value = self.sess.run(self.cost, feed_dict={
+                                    self.x: batch_x,
+                                    self.x_length: batch_x_length,
+                                    self.y: batch_y,
+                                    self.y_length: batch_y_length
+                                })
+
+                    print("epoch:", epoch, "- (", batch, "/", self.batch_size, ") -", cost_value)
 
     def predict(self, sentence):
+        # TODO
         pass
+
+    @staticmethod
+    def random_mini_batches(data_lists, mini_batch_size):
+        m = data_lists[0].shape[0]
+        mini_batches = []
+
+        # shuffle data
+        permutation = list(np.random.permutation(m))
+        shuffled = []
+        for data in data_lists:
+            shuffled.append(data[permutation])
+
+        mini_batch_number = int(m / float(mini_batch_size))
+
+        # split into mini batches
+        for data in shuffled:
+            tmp = []
+            for batch in range(0, mini_batch_number):
+                tmp.append(data[batch * mini_batch_size: (batch + 1) * mini_batch_size])
+
+            if m % mini_batch_size != 0:
+                tmp.append(data[mini_batch_number * mini_batch_size:])
+
+            mini_batches.append(tmp)
+
+        return mini_batches
--- Backend/WordEmbedding.py
+++ Backend/WordEmbedding.py
@@ -6,6 +6,9 @@ words = None
 words_to_index = None
 embeddings = None
 
+start = 0
+end = 0
+
 
 def create_embedding(glove_path):
     global words
@@ -21,23 +24,30 @@ def create_embedding(glove_path):
         word = split_line[0]
         embedding = [float(val) for val in split_line[1:]]
         words.append(word)
-        embedding.extend([0, 0, 0])
+        embedding.extend([0, 0, 0, 0])
         embeddings.append(embedding)
         words_to_index[word] = index
 
     words_to_index["<UNK>"] = len(words)
     words.append("<UNK>")
-    unk = [[0] * 50, [1], [0], [0]]
+    unk = [[0] * 50, [1], [0], [0], [0]]
     embeddings.append(unk)
 
     words_to_index["<PAD>"] = len(words)
     words.append("<PAD>")
-    pad = [[0] * 50, [0], [1], [0]]
+    pad = [[0] * 50, [0], [1], [0], [0]]
     embeddings.append(pad)
 
     words_to_index["<EOS>"] = len(words)
+    end = len(words)
     words.append("<EOS>")
-    eos = [[0] * 50, [0], [0], [1]]
+    eos = [[0] * 50, [0], [0], [1], [0]]
+    embeddings.append(eos)
+
+    words_to_index["<GO>"] = len(words)
+    start = len(words)
+    words.append("<GO>")
+    eos = [[0] * 50, [0], [0], [0], [1]]
     embeddings.append(eos)
 
     embeddings = np.array(embeddings)
